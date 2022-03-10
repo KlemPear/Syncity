10d5d8ac8a6bb5af10f630dc7656d263c031741b
KlemPear <57634684+KlemPear@users.noreply.github.com>
2022-03-09 23:16:22 -0700

    recreate frontend with react and not next.js

--- backend/index.js
+++ backend/index.js
@@ -21,5 +21,5 @@ app.use("*", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port: http://localhost:${port}/`);
+	console.log(`Listening on port: http://localhost:${port}/`);
 });
