a37e967acc6d329c5597e5857b0d75d2c2c47337
KlemPear <57634684+KlemPear@users.noreply.github.com>
2022-03-17 21:30:28 -0600

    remove / at the end of prod URL

--- frontend/src/apis/baseUrl.js
+++ frontend/src/apis/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl =
 	process.env.NODE_ENV === "production"
-		? "https://syncity.herokuapp.com/"
+		? "https://syncity.herokuapp.com"
 		: "http://localhost:5000";
 export default baseUrl;
