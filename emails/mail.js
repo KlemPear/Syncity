b861bee7067d3d817fc66037c8542f350ce7f0a5
KlemPear <57634684+KlemPear@users.noreply.github.com>
2022-04-25 23:09:04 -0600

    add sendgrid emailing

--- emails/mail.js
+++ emails/mail.js
@@ -1,31 +1,42 @@
 const nodemailer = require("nodemailer");
-const { google } = require("googleapis");
-const OAuth2 = google.auth.OAuth2;
 
-const oauth2Client = new OAuth2(
-	process.env.OAUTH_CLIENTID, // ClientID
-	process.env.OAUTH_CLIENT_SECRET, // Client Secret
-	"https://developers.google.com/oauthplayground" // Redirect URL
-);
+//Google GMAIL
+// const { google } = require("googleapis");
+// const OAuth2 = google.auth.OAuth2;
 
-oauth2Client.setCredentials({
-	refresh_token: process.env.OAUTH_REFRESH_TOKEN,
-});
-const accessToken = oauth2Client.getAccessToken();
+// const oauth2Client = new OAuth2(
+// 	process.env.OAUTH_CLIENTID, // ClientID
+// 	process.env.OAUTH_CLIENT_SECRET, // Client Secret
+// 	"https://developers.google.com/oauthplayground" // Redirect URL
+// );
+
+// oauth2Client.setCredentials({
+// 	refresh_token: process.env.OAUTH_REFRESH_TOKEN,
+// });
+// const accessToken = oauth2Client.getAccessToken();
+
+// const transporter = nodemailer.createTransport({
+// 	service: "gmail",
+// 	auth: {
+// 		type: "OAuth2",
+// 		user: process.env.MAIL_USERNAME,
+// 		pass: process.env.MAIL_PASSWORD,
+// 		clientId: process.env.OAUTH_CLIENTID,
+// 		clientSecret: process.env.OAUTH_CLIENT_SECRET,
+// 		refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+// 		accessToken: accessToken,
+// 	},
+// 	tls: {
+// 		rejectUnauthorized: false,
+// 	},
+// });
 
 const transporter = nodemailer.createTransport({
-	service: "gmail",
+	host: "smtp.sendgrid.net",
+	port: 587,
 	auth: {
-		type: "OAuth2",
-		user: process.env.MAIL_USERNAME,
-		pass: process.env.MAIL_PASSWORD,
-		clientId: process.env.OAUTH_CLIENTID,
-		clientSecret: process.env.OAUTH_CLIENT_SECRET,
-		refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-		accessToken: accessToken,
-	},
-	tls: {
-		rejectUnauthorized: false,
+		user: "apikey",
+		pass: process.env.SENDGRID_API_KEY,
 	},
 });
 
@@ -39,7 +50,7 @@ transporter.verify(function (error, success) {
 
 const sendEmail = (options) => {
 	const mailOptions = {
-		from: options.fromAddress ?? "do-not-reply@akapela.com",
+		from: options.fromAddress ?? "do-not-reply@akapela.co",
 		to: options.toAddress ?? "",
 		subject: options.subject ?? "",
 		text: options.text ?? "",
