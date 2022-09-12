16d07ffa0daae2bf9f7f7a25940d7e739a723ffe
KlemPear <57634684+KlemPear@users.noreply.github.com>
2022-09-11 21:42:18 -0600

    fix profile

--- frontend/src/components/Users/ShowUser.js
+++ frontend/src/components/Users/ShowUser.js
@@ -12,7 +12,25 @@ import UserSearch from "./UserSearch";
 import payments from "../../apis/payments";
 
 //mui
-import { Button, Typography, Box, Divider, List, ListItemText } from "@mui/material";
+import {
+	Button,
+	Typography,
+	Box,
+	Divider,
+	List,
+	ListItemText,
+	Stack,
+} from "@mui/material";
+
+const PlanFormatterDict = {
+	freePlan: "Free Plan",
+	basicBriefPlan: "Basic Brief Plan",
+	proBriefPlan: "Pro Brief Plan",
+	businessBriefPlan: "Business Brief Plan",
+	basicPitchPlan: "Basic Pitch Plan",
+	proPitchPlan: "Pro Pitch Plan",
+	businessPitchPlan: "Business Pitch Plan",
+};
 
 class ShowUser extends React.Component {
 	constructor(props) {
@@ -24,10 +42,6 @@ class ShowUser extends React.Component {
 		this.props.cleanSearchedUser();
 		this.setState({ showModal: false });
 	};
-
-	// onMoreTokensClick = () => {
-	// 	this.props.addTokensToUser(this.props.currentUser._id, 10);
-	// };
 
 	onAddConnectionsClick = () => {
 		this.setState({ showModal: true });
@@ -79,133 +93,155 @@ class ShowUser extends React.Component {
 
 	renderUserProfile() {
 		return (
-			<Box
-				sx={{
-					display: "flex",
-					justifyContent: "space-evenly",
-					alignItems: "center",
-					flexDirection: "column",
-					minHeight: 500,
-				}}
-			>
-				<Typography
-					variant="h2"
-					sx={{
-						fontWeight: 700,
-						letterSpacing: ".2rem",
-						color: "inherit",
-						textDecoration: "none",
-					}}
-				>
-					{this.props.currentUser.firstName} {this.props.currentUser.lastName}
-				</Typography>
-				<Typography>Email: {this.props.currentUser.email}</Typography>
-				<Typography>{this.props.currentUser.bio}</Typography>
-				<Divider variant="middle" sx={{ margin: 1 }} />
-				<Box
-					sx={{
-						justifyContent: "space-evenly",
-					}}
-				>
-					<Typography sx={{ mb: 2 }} variant="h5">
-						Current Subscription Plans
+			<Box sx={{ display: "flex", justifyContent: "center" }}>
+				<Stack spacing={2}>
+					<Typography
+						variant="h2"
+						sx={{
+							fontWeight: 700,
+							letterSpacing: ".2rem",
+							color: "inherit",
+							textDecoration: "none",
+							display: "flex",
+							justifyContent: "center",
+						}}
+					>
+						{this.props.currentUser.firstName} {this.props.currentUser.lastName}
 					</Typography>
-					<ul>
-						<li>
-							Brief Plan:{" "}
-							{this.props.currentUser.briefSubscriptionPlan ?? "no plan"},{" "}
-							{this.props.currentUser.briefTokens === -1
-								? "unlimited"
-								: this.props.currentUser.briefTokens}{" "}
-							briefs remaining for this billing period.
-						</li>
-						<li>
-							Pitch Plan:{" "}
-							{this.props.currentUser.pitchSubscriptionPlan ?? "no plan"},{" "}
-							{this.props.currentUser.pitchTokens === -1
-								? "unlimited"
-								: this.props.currentUser.pitchTokens}{" "}
-							pitches remaining for this billing period.
-						</li>
-					</ul>
-
-					<Box sx={{ mt: 2 }}>
-						{this.props.currentUser.stripeCustomerId ? (
-							<form
-								onSubmit={(event) =>
-									this.onStripeBillingSubmit(
-										event,
-										this.props.currentUser.stripeCustomerId
-									)
-								}
-							>
-								<Button
-									variant="contained"
-									color="secondary"
-									id="checkout-and-portal-button"
-									type="submit"
-								>
-									Manage your billing information
-								</Button>
-							</form>
-						) : (
-							<Button
-								variant="contained"
-								color="secondary"
-								component={Link}
-								to="/buy-tokens"
-							>
-								Subscribe to a plan
-							</Button>
-						)}
-					</Box>
-				</Box>
-				<Divider variant="middle" sx={{ margin: 1 }} />
-				<Box
-					spacing={2}
-					sx={{
-						justifyContent: "space-evenly",
-					}}
-				>
-					<Typography sx={{ mb: 2 }} variant="h5">
-						Connections
+					<Typography sx={{ display: "flex", justifyContent: "center" }}>
+						Email: {this.props.currentUser.email}
 					</Typography>
+					<Typography sx={{ display: "flex", justifyContent: "center" }}>
+						{this.props.currentUser.bio}
+					</Typography>
+					<Divider variant="middle" sx={{ margin: 1, bgcolor: "black" }} />
 					<Box
 						sx={{
 							display: "flex",
+							justifyContent: "space-evenly",
 							flexDirection: "column",
-							maxHeight: 200,
-							overflow: "hidden",
-							overflowY: "scroll",
 						}}
 					>
-						<List dense>
-						{this.props.currentUser.connections
-							? this.props.currentUser.connections.map((connection) => (
-									<ListItemText
-										key={connection._id}
-									>{`${connection.firstName} ${connection.lastName} - ${connection.email}`}</ListItemText>
-							  ))
-							: null}
-						</List>
+						<Typography sx={{ mb: 2 }} variant="h5">
+							Current Subscription Plans
+						</Typography>
+						<ul>
+							<li>
+								<Typography variant="h6">
+									Brief Plan:{" "}
+									{this.props.currentUser.briefSubscriptionPlan
+										? PlanFormatterDict[
+												this.props.currentUser.briefSubscriptionPlan
+										  ]
+										: "No Plan."}
+								</Typography>
+								<ul>
+									<li>
+										{this.props.currentUser.briefTokens === -1
+											? "unlimited"
+											: this.props.currentUser.briefTokens}{" "}
+										briefs remaining for this billing period.
+									</li>
+								</ul>
+							</li>
+							<li>
+								<Typography variant="h6">
+									Pitch Plan:{" "}
+									{this.props.currentUser.pitchSubscriptionPlan
+										? PlanFormatterDict[
+												this.props.currentUser.pitchSubscriptionPlan
+										  ]
+										: "no plan"}
+								</Typography>
+								<ul>
+									<li>
+										{this.props.currentUser.pitchTokens === -1
+											? "unlimited"
+											: this.props.currentUser.pitchTokens}{" "}
+										pitches remaining for this billing period.
+									</li>
+								</ul>
+							</li>
+						</ul>
+
+						<Box sx={{ mt: 2 }}>
+							{this.props.currentUser.stripeCustomerId ? (
+								<form
+									onSubmit={(event) =>
+										this.onStripeBillingSubmit(
+											event,
+											this.props.currentUser.stripeCustomerId
+										)
+									}
+								>
+									<Button
+										variant="contained"
+										color="secondary"
+										id="checkout-and-portal-button"
+										type="submit"
+									>
+										Manage your billing information
+									</Button>
+								</form>
+							) : (
+								<Button
+									variant="contained"
+									color="secondary"
+									component={Link}
+									to="/buy-tokens"
+								>
+									Subscribe to a plan
+								</Button>
+							)}
+						</Box>
 					</Box>
-					<Button
-						variant="contained"
-						color="secondary"
-						className="ui classic blue button"
-						onClick={() => this.onAddConnectionsClick()}
-						sx={{ mt: 2 }}
+					<Divider variant="middle" sx={{ margin: 1, bgcolor: "black" }} />
+					<Box
+						spacing={2}
+						sx={{
+							justifyContent: "space-evenly",
+						}}
 					>
-						Find new connections
-					</Button>
-					<TransitionModal
-						showModal={this.state.showModal}
-						title={"Find new connections"}
-						content={this.renderModalContent()}
-						actions={this.renderModalActions()}
-						onDismiss={() => this.onDismissModal()}
-					/>
-				</Box>
+						<Typography sx={{ mb: 2 }} variant="h5">
+							Connections
+						</Typography>
+						<Box
+							sx={{
+								display: "flex",
+								flexDirection: "column",
+								maxHeight: 200,
+								overflow: "hidden",
+								overflowY: "auto",
+							}}
+						>
+							<List dense>
+								{this.props.currentUser.connections
+									? this.props.currentUser.connections.map((connection) => (
+											<ListItemText
+												key={connection._id}
+											>{`${connection.firstName} ${connection.lastName} - ${connection.email}`}</ListItemText>
+									  ))
+									: null}
+							</List>
+						</Box>
+						<Button
+							variant="contained"
+							color="secondary"
+							className="ui classic blue button"
+							onClick={() => this.onAddConnectionsClick()}
+							sx={{ mt: 2 }}
+						>
+							Find new connections
+						</Button>
+						<TransitionModal
+							showModal={this.state.showModal}
+							title={"Find new connections"}
+							content={this.renderModalContent()}
+							actions={this.renderModalActions()}
+							onDismiss={() => this.onDismissModal()}
+						/>
+					</Box>
+				</Stack>
 			</Box>
 		);
 	}
