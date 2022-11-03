bbc007ae94d6a4e6e0fc8eaf69a5c745507b15a3
KlemPear <57634684+KlemPear@users.noreply.github.com>
2022-11-03 16:04:49 -0600

    clean branch

--- emails/mail.js
+++ emails/mail.js
@@ -1,54 +1,5 @@
-const nodemailer = require("nodemailer");
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-//Google GMAIL
-// const { google } = require("googleapis");
-// const OAuth2 = google.auth.OAuth2;
-
-// const oauth2Client = new OAuth2(
-// 	process.env.OAUTH_CLIENTID, // ClientID
-// 	process.env.OAUTH_CLIENT_SECRET, // Client Secret
-// 	"https://developers.google.com/oauthplayground" // Redirect URL
-// );
-
-// oauth2Client.setCredentials({
-// 	refresh_token: process.env.OAUTH_REFRESH_TOKEN,
-// });
-// const accessToken = oauth2Client.getAccessToken();
-
-// const transporter = nodemailer.createTransport({
-// 	service: "gmail",
-// 	auth: {
-// 		type: "OAuth2",
-// 		user: process.env.MAIL_USERNAME,
-// 		pass: process.env.MAIL_PASSWORD,
-// 		clientId: process.env.OAUTH_CLIENTID,
-// 		clientSecret: process.env.OAUTH_CLIENT_SECRET,
-// 		refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-// 		accessToken: accessToken,
-// 	},
-// 	tls: {
-// 		rejectUnauthorized: false,
-// 	},
-// });
-
-// const transporter = nodemailer.createTransport({
-// 	host: "smtp.sendgrid.net",
-// 	port: 587,
-// 	auth: {
-// 		user: "apikey",
-// 		pass: process.env.SENDGRID_API_KEY,
-// 	},
-// });
-
-// transporter.verify(function (error, success) {
-// 	if (error) {
-// 		console.log(error);
-// 	} else {
-// 		console.log("Email Server is ready to send our messages");
-// 	}
-// });
 
 const sendEmail = (options) => {
 	const mailOptions = {
@@ -61,13 +12,6 @@ const sendEmail = (options) => {
 		dynamicTemplateData: options.dynamicTemplateData ?? {},
 	};
 
-	// transporter.sendMail(mailOptions, function (err, data) {
-	// 	if (err) {
-	// 		console.log("Error " + err);
-	// 	} else {
-	// 		console.log("Email sent successfully: " + data.response);
-	// 	}
-	// });
 	sgMail
 		.send(mailOptions)
 		.then(() => {
