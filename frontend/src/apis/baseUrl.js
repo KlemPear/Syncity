a8f5cc4caec0c1843112e2e71884e7ba79138de6
KlemPear <57634684+KlemPear@users.noreply.github.com>
2023-04-28 12:48:10 -0600

    update baseUrl

--- frontend/src/apis/baseUrl.js
+++ frontend/src/apis/baseUrl.js
@@ -1,7 +1,18 @@
-const baseUrl =
-  process.env.NODE_ENV === "production"
-    ? "https://app.nost.audio"
-    : process.env.NODE_ENV === "staging"
-    ? "https://syncity-staging.herokuapp.com/"
-    : "http://localhost:8080";
+// const baseUrl =
+//   process.env.NODE_ENV === "production"
+//     ? "https://app.nost.audio"
+//     : process.env.NODE_ENV === "staging"
+//     ? "https://syncity-staging.herokuapp.com"
+//     : "http://localhost:8080";
+let baseUrl = "";
+switch(process.env.NODE_ENV) {
+	case "production":
+		baseUrl = "https://app.nost.audio";
+		break;
+	case "staging":
+		baseUrl = "https://syncity-staging.herokuapp.com";
+		break;
+	default:
+		baseUrl = "http://localhost:8080";
+}
 export default baseUrl;
